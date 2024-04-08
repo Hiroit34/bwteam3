abec9c098d3acd1017cfc2cdbc7c9cc4938a112e
Hiroit34 <franz.natavio@gmail.com>
2024-04-08 16:29:13 +0200

    Co-authored-by: DanieleLuise <DanieleLuise@users.noreply.github.com> Co-authored-by: Simone Iengo <ilmuratore@users.noreply.github.com>

--- profumiDiBacco/src/app/auth/register/register.component.ts
+++ profumiDiBacco/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IUser } from '../../Modules/i-user';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  registerData: Partial<IUser> = {}
+
+  constructor(
+    private authSvc: AuthService,
+    private router: Router
+  ) { }
+
+  signUp() {
+    this.authSvc.register(this.registerData)
+      .subscribe(data => {
+
+        this.router.navigate(['dashboard'])
+
+      })
+  }
 
 }
+
+
+

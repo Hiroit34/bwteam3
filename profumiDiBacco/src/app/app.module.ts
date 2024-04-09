79fde2180cf9bdb5d5fc12d0a259a44f59539fec
yohannesluminari <y.luminari@gmail.com>
2024-04-09 15:20:06 +0200

    contatti chi siamo fuck

--- profumiDiBacco/src/app/app.module.ts
+++ profumiDiBacco/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ContattiComponent } from './pages/contatti/contatti.component';
 import { ChiSiamoComponent } from './pages/chi-siamo/chi-siamo.component';
 import { FaqComponent } from './pages/faq/faq.component';
@@ -26,7 +26,8 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbCollapseModule
  ],
   providers: [],
   bootstrap: [AppComponent]

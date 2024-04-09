4b97923993632afb25b6050a4a53d8097bbddaa1
ha <iengo.simone@gmail.com>
2024-04-10 00:35:33 +0200

    search service implemented at 50%

--- profumiDiBacco/src/app/main-components/header/header.component.ts
+++ profumiDiBacco/src/app/main-components/header/header.component.ts
@@ -1,31 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { SearchService } from '../../services/search.service';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
-  constructor(private authSvc:AuthService){}
+  constructor(private authSvc:AuthService, private searchService: SearchService){}
   show: boolean = false
-
   logged:boolean= false
+
+
+  search(query: string){
+    this.searchService.changeSearchQuery(query)
+  }
+
 
   ngOnInit(){
 
-    this.authSvc.isLoggedIn$.subscribe(data=>{
-
-      this.logged = data
-
-    })
-
-
+    this.authSvc.isLoggedIn$.subscribe(data=>{this.logged = data})
   }
-  logout(){
 
-    this.authSvc.logout()
 
-  }
+  logout(){this.authSvc.logout()};
 }
---
+++ profumiDiBacco/src/app/services/search.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { IWine } from '../Modules/i-wine';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  private url = 'http://localhost:3000/wines';
+  private searchQuery = new BehaviorSubject<string>(''); // permette l'aggiornamento della query di ricerca  
+  currentSearchQuery = this.searchQuery.asObservable(); // permette di osservare la query di ricerca corrente 
+  constructor (private http: HttpClient) {}
+
+  changeSearchQuery(query: string) { // questo metodo non sostituisce searchWines, ma permette di aggiornare la query di ricerca rendendo possibile la ricerca in tempo reale 
+    this.searchQuery.next(query);     // con aggiornamento dinamico della card del risultato. 
+  }
+
+  searchWines(query = ''): Observable<IWine[]> {
+    return this.http.get<IWine[]>(`${this.url}?q=${query}`);
+  }
+}

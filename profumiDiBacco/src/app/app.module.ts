b31ff82e6e733dc5a619fe22a8799f11afcb07a2
Simone Giarnera <simone.giarnera@gmail.com>
2024-04-10 22:40:43 +0200

    creation style homepage

--- profumiDiBacco/src/app/app.module.ts
+++ profumiDiBacco/src/app/app.module.ts
@@ -3,7 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbCollapseModule, NgbDatepickerModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbCollapseModule,
+  NgbDatepickerModule,
+  NgbModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { ContattiComponent } from './pages/contatti/contatti.component';
 import { ChiSiamoComponent } from './pages/chi-siamo/chi-siamo.component';
 import { FaqComponent } from './pages/faq/faq.component';
@@ -11,6 +15,7 @@ import { HeaderComponent } from './main-components/header/header.component';
 import { FooterComponent } from './main-components/footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BannerComponent } from './main-components/banner/banner.component';
 
 @NgModule({
   declarations: [
@@ -27,9 +32,9 @@ import { HttpClientModule } from '@angular/common/http';
     NgbModule,
     FormsModule,
     HttpClientModule,
-    NgbCollapseModule
-],
+    NgbCollapseModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- profumiDiBacco/src/app/pages/home/home.module.ts
+++ profumiDiBacco/src/app/pages/home/home.module.ts
@@ -4,16 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { WineCardComponent } from '../../main-components/wine-card/wine-card.component';
-
+import { BannerComponent } from '../../main-components/banner/banner.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    WineCardComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent, WineCardComponent, BannerComponent],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}

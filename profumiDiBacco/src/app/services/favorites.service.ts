00000aa3638fb9b2d460fa671e831defdb6d50ad
nadiasemprebon <semprebonnadia6@gail.com>
2024-04-10 18:01:40 +0200

    da sistemare la sovrascrizione

--- profumiDiBacco/src/app/services/favorites.service.ts
+++ profumiDiBacco/src/app/services/favorites.service.ts
@@ -5,45 +5,53 @@ import { AuthService } from '../auth/auth.service';
 import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FavoritesService {
   private favorites: IWine[] = [];
   private dbUrl = 'http://localhost:3000/users';
-  private favoritesUrl = 'http://localhost:3000/favorites'
-  constructor(
-    private http: HttpClient,
-    private authService: AuthService
-  ) { }
-
-  isFavorite(wine: IWine): boolean {
-    return this.favorites.some(favoriteWine => favoriteWine.id === wine.id);
+  private favoritesUrl = 'http://localhost:3000/favorites';
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.authService.isLoggedIn$.subscribe((res) => {
+      if (!res) this.favorites = [];
+    });
   }
 
-  addtoFavorites(wine: IWine): void{
-    const isFavorite = this.favorites.some(favoriteWine => favoriteWine.id === wine.id)
+  isFavorite(wine: IWine): boolean {
+    return this.favorites.some((favoriteWine) => favoriteWine.id === wine.id);
+  }
+
+  addtoFavorites(wine: IWine): void {
+    const isFavorite = this.favorites.some(
+      (favoriteWine) => favoriteWine.id === wine.id
+    );
 
     this.favorites.push(wine);
     const userId = this.authService.getUserId(); // Assuming there is a method in the AuthService to get the userId
-    this.http.patch(`${this.dbUrl}/${userId}`, {favoriteWine: this.favorites}).subscribe();
+    this.http
+      .patch(`${this.dbUrl}/${userId}`, { favoriteWine: this.favorites })
+      .subscribe();
   }
 
   removeFromFavorites(wine: IWine): void {
-    const index = this.favorites.findIndex(favoriteWine => favoriteWine.id === wine.id);
-     this.favorites.splice(index, 1);
+    const index = this.favorites.findIndex(
+      (favoriteWine) => favoriteWine.id === wine.id
+    );
+    this.favorites.splice(index, 1);
 
-     const userId = this.authService.getUserId();
-     if(userId){
-        this.http.patch(`${this.dbUrl}/${userId}`, {favoriteWine: this.favorites}).subscribe();
-     }
+    const userId = this.authService.getUserId();
+    if (userId) {
+      this.http
+        .patch(`${this.dbUrl}/${userId}`, { favoriteWine: this.favorites })
+        .subscribe();
+    }
   }
 
   setFavorites(favorites: IWine[]): void {
     this.favorites = favorites;
   }
 
-  getFavorites(): IWine[]{
-    return this.favorites;  
+  getFavorites(): IWine[] {
+    return this.favorites;
   }
-
 }
